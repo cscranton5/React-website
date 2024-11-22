e624fe4ca0f456640c3c534293f03437f7f0ceaa
Carter Scranton <cgs2xw@mst.edu>
2024-11-22 15:45:54 -0600

    Bord basic function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    Board()
   );
 }
+
+
+
+export function Board() {
+
+  const [squares,setSquares] = useState(Array(9).fill(null))
+
+
+  return (
+    <>
+      <div>
+        <div className='Tic-tack-toe-board'>
+          <div className='board-row'>
+            <Square value={squares[0]} onSquareClicked={onClick}/>
+            <Square value={squares[1]} onSquareClicked={onClick}/>
+            <Square value={squares[2]} onSquareClicked={onClick}/>
+          </div>
+          <div className='board-row'>
+            <Square value={squares[3]} onSquareClicked={onClick}/>
+            <Square value={squares[4]} onSquareClicked={onClick}/>
+            <Square value={squares[5]} onSquareClicked={onClick}/>
+          </div>
+          <div className='board-row'>
+            <Square value={squares[6]} onSquareClicked={onClick}/>
+            <Square value={squares[7]} onSquareClicked={onClick}/>
+            <Square value={squares[8]} onSquareClicked={onClick}/>
+          </div>
+        </div>
+      </div>
+
+
+    </>
+
+  );
+}
+
+function Square({value, onSquareClicked}) {
+
+  function handleClick() {
+    setValue('X');
+  }
+
+
+  return <button className="square" onClick={onSquareClicked}>{value}</button>;
+} 
 
 export default App;
